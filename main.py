4fa12975ffe35e3076553efe34689e9d4aef897b
Ozkan Ozsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2022-11-13 13:44:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+
+
+
+names = "pizza, french fries"
+print(names)
+
+age = '44'
+print(age)
+
+a = 18
+b = 21
+
+if a < b:
+    print("a is smaller than b")
+    
+else:
+        
+        print("b is not bigger than a")
